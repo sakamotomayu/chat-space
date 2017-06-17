f1f8374ba7843a9842d888c8bd53c3ea5044805a
mayu <sakamotomayu0201@gmail.com>
2017-06-17 17:25:18 +0900

    Add edit & update action
    edit&updateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    current_user.update(update_params)
+  end
+
+  private
+    params.require(:user).permit(:email,:name)
+  end
 end
