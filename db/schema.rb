4e17df9f264979e4b37bd24a7c2309c8af96ba12
mayu <sakamotomayu0201@gmail.com>
2017-06-17 09:19:29 +0900

    Reinstall devise
    Reinstall devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610071548) do
+ActiveRecord::Schema.define(version: 20170617001352) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
