a4d8d0793f7b68fc2fe1562028de5b27d5a45257
mayu <sakamotomayu0201@gmail.com>
2017-06-18 11:31:32 +0900

    Not create helper file
    ヘルパー関連のファイルが作成されないように設定した

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module ChatSpace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators.helper = false
     config.i18n.default_locale = :ja
   end
 end
