ebdb23179dd6b1c0ca969223072d01eb18ec75cc
mayu <sakamotomayu0201@gmail.com>
2017-08-08 09:58:24 +0900

    Edit routes.rb
    ネストに関する誤記があったため修正した。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users ,only: [:edit,:update]
-  resources :groups
+  resources :groups do
     resources :messages
+  end
 end
