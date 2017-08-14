6d4a1f8e3f09488d0e6a8c1e4c4ef965f2cef6c0
mayu <sakamotomayu0201@gmail.com>
2017-08-14 21:47:30 +0900

    Delete @messages
    @messagesを削除した。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,6 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
     @message = Message.new
-    @messages = @group.messages
     @users = @group.users
   end
 
