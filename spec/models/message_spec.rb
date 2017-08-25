8a85f2dc17985b3e415f33c198d34a0f9d304277
mayu <sakamotomayu0201@gmail.com>
2017-08-25 18:58:56 +0900

    Create spec-file
    specファイル作成

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,45 +2,38 @@ require 'rails_helper'
 
 describe Message do
   describe '#create' do
-    it "is valid with a text, image, user_id, group_id" do
+    it 'is valid with a text' do
       message = build(:message)
       expect(message).to be_valid
     end
 
-    it "is invalid without a text" do
-      message = build(:message, text: "")
+    it 'is valid with a image' do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    it 'is valid with a text, image' do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    it 'is invalid without a text, image' do
+      message = build(:message, text: nil, image: nil)
       message.valid?
-      expect(message.errors[:text]).to include("を入力してください")
+      expect(message.errors[:text, :image]).to include("メッセージを入力してください")
     end
 
-    it "is valid with a same number user_id and user.id" do
-      user = build(:user)
-      message = build(:message, user_id: user.id)
-      expect(message.user_id).to eq user.id
-    end
-
-    it "is valid with a same number group_id and group.id" do
-      group = build(:group)
-      message = build(:message, group_id: group.id)
-      expect(message.group_id).to eq group.id
-    end
-
-    it "is invalid with a text that has more than 255 characters " do
-      message = build(:message, text: "a"*256)
+    it 'is invalid without a group_id' do
+      message = build(:message, group_id: nil)
       message.valid?
-      expect(message.errors[:text][0]).to include("は255文字以内で入力してください")
+      expect(message.errors[:group_id]).to include("can't be blank")
     end
 
-    it "is invalid without a user_id" do
-      message = build(:message, user_id: "")
+    it 'is invalid without a user_id' do
+      message = build(:message, user_id: nil)
       message.valid?
-      expect(message.errors[:user_id]).to include("を入力してください")
+      expect(message.errors[:user_id]).to include("can't be blank")
     end
 
-    it "is invalid without a group_id" do
-      message = build(:message, group_id: "")
-      message.valid?
-      expect(message.errors[:group_id]).to include("を入力してください")
-    end
   end
 end
